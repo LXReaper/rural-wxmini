f3f87043c2679fc3618ba07f62c7bf8f54e03c31
pyj1234 <1326126225@qq.com>
2024-03-31 14:47:14 +0800

    编写了框架

---
+++ pages/mine/mine.js
@@ -0,0 +1,11 @@
+// mine.js - 我的页面逻辑
+
+Page({
+    data: {
+
+    },
+    onLoad: function () {
+        // 加载我的数据
+    },
+    // 其他逻辑
+})
---
+++ pages/score/score.js
@@ -0,0 +1,11 @@
+// index.js - 首页逻辑
+
+Page({
+    data: {
+        // 数据
+    },
+    onLoad: function () {
+        // 页面加载
+    },
+    // 其他逻辑
+})
