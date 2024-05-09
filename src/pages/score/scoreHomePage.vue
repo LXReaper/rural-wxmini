aad4ff0c19c1223fb941133849d5cc28aade8594
练庆锦 <1661222469@qq.cpm>
2024-05-09 22:35:54 +0800

    添加了学习中心，排行榜，任务中心，文明家庭四个按钮，添加了学习中心的空白页面

--- src/pages/score/scoreHomePage.vue
+++ src/pages/score/scoreHomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <view>
+  <scroll-view scroll-y="true" bindscroll="handleScroll" scroll-with-animation="true"><!--可滑动页面-->
     <!-- 圆角矩形容器 -->
     <view class="rounded-container">
       <!-- 六个小按钮 -->
@@ -10,11 +10,22 @@
       <view class="button" style="background-color: #ff00ff;">签到</view>
       <view class="button" style="background-color: #00ffff;">考察</view>
     </view>
-  </view>
+    <!--下方四个按钮-->
+    <view style="display:flex" id="button2">
+      <button class="button1" style="background-color: #04cefc" @click="LearningCenter">学习中心</button>
+      <button class="button2" style="background-color: #4cd964">任务中心</button>
+      <button class="button3" style="background-color: #007aff">排行榜</button>
+      <button class="button4" style="background-color: #04fefc">文明家庭</button>
+    </view>
+  </scroll-view>
 </template>
 
-<script setup lang="ts">
-
+<script setup >
+const  LearningCenter = () =>{
+uni.navigateTo({
+  url:"/pages/score/LearningCenter"
+});
+};
 </script>
 
 <style scoped lang="scss">
@@ -27,37 +38,51 @@
 }
 
 .button {
-  width: 30px;
+  width: auto; /* 自适应宽度 */
   height: 30px;
   margin-right: 10px;
   border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 0.8em; /* 使用相对单位设置文本大小 */
 }
 
-.button:nth-child(1) {
-  background-color: #ff0000;
-  font-size: 0.8em; /* 使用相对单位设置文本大小 */
+
+#button2 {
+  display: flex;
 }
-.button:nth-child(2) {
-  background-color:  #00ff00;
+.button1,
+.button2,
+.button3,
+.button4 {
+  flex: 1;
+  height: 30px;
+  margin-right: 10px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #04cefc; /* 默认背景颜色 */
+  color: white; /* 文本颜色 */
   font-size: 0.8em; /* 使用相对单位设置文本大小 */
+  white-space: nowrap; /* 防止文本换行 */
+  overflow: hidden; /* 隐藏溢出部分文本 */
+  text-overflow: ellipsis; /* 显示省略号 */
 }
-.button:nth-child(3) {
-  background-color:  #0000ff;
-  font-size: 0.8em; /* 使用相对单位设置文本大小 */
+
+.button2 {
+  background-color: #4cd964;
 }
-.button:nth-child(4) {
-  background-color:  #ffff00;
-  font-size: 0.8em; /* 使用相对单位设置文本大小 */
+
+.button3 {
+  background-color: #007aff;
 }
-.button:nth-child(5) {
-  background-color: #ff00ff;
-  font-size: 0.8em; /* 使用相对单位设置文本大小 */
+
+.button4 {
+  background-color: #04fefc;
 }
-.button:nth-child(6) {
-  background-color: #00ffff;
-  font-size: 0.8em; /* 使用相对单位设置文本大小 */
-}
+
 </style>
+
+}
