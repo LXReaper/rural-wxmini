c4ba52651176d029215af34c53f49ded2878510f
阮梓杰 <qy2131467r@163.com>
2024-05-10 21:44:20 +0800

    1

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,13 +1,14 @@
 <template>
 
       <view class="contact-background">
-        <uni-icons class="uniui-person" type="contact" size="17" color="green">个人中心</uni-icons>
+        <uni-icons class="uniui-person" type="contact" size="15" color="green"
+                   @click="personal">个人中心</uni-icons>
       </view>
 
     <view class="the-information-buttons">
 
       <view class="half-up">
-        <image class="head" src="/static/logo.png"></image>
+        <image class="head" src="./static/store/lqj.jpg"></image>
         <view class="the-text">
           <text>用户名：102yes{{userName}}</text>
           <text>\n用户ID：114514{{userId}}</text>
@@ -17,102 +18,93 @@
       </view>
 
       <view class="two-choice">
-        <view class="index-record">积分记录</view>
-        <view class="information">我的信息</view>
+        <button class="index-record">积分记录</button>
+        <button class="information">我的信息</button>
       </view>
+
       <view class="down-page">
-        <view class="goodgood-bank">
-          <image class="icon" src="./static/mine/record.png"></image> <!-- 图标放在文本左边 -->
-          美德银行
-        </view>
-        <view class="release-task">发布任务</view>
-        <view class="record">积分记录</view>
-        <view class="online-service">在线客服</view>
-        <view class="feedback-consultation">咨询和反馈</view>
-        <view class="setting">设置</view>
+        <button class="goodgood-bank">美德银行</button>
+        <button  class="release-task">发布任务</button>
+        <button class="record">积分记录</button>
+        <button class="online-service">在线客服</button>
+        <button class="feedback-consultation">咨询和反馈</button>
+
+        <button class="setting" @click="settingClick">设置</button>
       </view>
     </view>
 
 </template>
 
-<script>
-import UniIcons from "../../uni_modules/uni-icons/components/uni-icons/uni-icons.vue";
-
-export default {
-  components: {UniIcons},
-  data() {
-    return {};
-  }
-}
+<script setup>
+const settingClick = () => {
+  uni.navigateTo({
+    url: "/pages/mine/setting",
+  });
+};
+const personal = () => {
+  uni.navigateTo({
+    url:"/pages/mine/personal-information",
+    });
+};
 </script>
 
 <style lang="scss">
 .uniui-person{
-  color: green;
+  color: green; margin-top: 2rpx;
 }
 .contact-background{
-  margin-top: 10rpx;
-  margin-left: 10rpx;
-  border: 2rpx solid grey;
-  border-radius: 20rpx;
+  margin-top: 10rpx;  margin-left: 10rpx;
+  border: 2rpx solid grey;  border-radius: 20rpx;
   background-color: white;
-  width: 200rpx;
-  height: 50rpx
-}
-.icon {
-  width: 50rpx; /* 设置图标宽度 */
-  height: 50rpx; /* 设置图标高度 */
-  margin-top: 10rpx; /* 调整图标与上方元素的间距 */
+  width: 190rpx;  height: 50rpx;
 }
 .head{
   border: white;
   border-radius: 160rpx;
   width:  150rpx;
   height: 150rpx;
-  margin-left: 4rpx;
-  margin-top: 4rpx;
+  margin-left: 12rpx;
+  margin-top: 12rpx;
 }
 .index-record {
-  margin-top: 20rpx;
-  height: 75rpx;
-  border-right: 4rpx solid #EFF3FF;
-  width: 300rpx;
+  width: 300rpx;  border-radius: 20rpx;
+
 }
 .information{
-  margin-top: 25rpx;
-  height: 75rpx;
-  width: 300rpx;
-  border-radius: 20rpx;
+  width: 300rpx;  border-radius: 20rpx;
+
 }
 
 .goodgood-bank {
   height: 85rpx;
-  line-height: 85rpx;
+  font-size: 39rpx;
+  line-height: 80rpx;
 }
 .release-task {
   height: 85rpx;
-  line-height: 85rpx;
-  border-top: 4rpx solid #EFF3FF;
+  font-size: 39rpx;
+  line-height: 80rpx;
 }
 .record {
   height: 85rpx;
-  line-height: 85rpx;
-  border-top: 4rpx solid #EFF3FF;
+  font-size: 39rpx;
+  line-height: 80rpx;
 }
 .online-service {
   height: 85rpx;
-  line-height: 85rpx;
-  border-top: 4rpx solid #EFF3FF;
+  font-size: 39rpx;
+  line-height: 80rpx;
+
  }
 .feedback-consultation {
   height: 85rpx;
-  line-height: 85rpx;
-  border-top: 4rpx solid #EFF3FF;
+  font-size: 39rpx;
+  line-height: 80rpx;
 }
 .setting {
-  height: 85rpx;
-  line-height: 85rpx;
-  border-top: 4rpx solid #EFF3FF;
+  height: 85rpx;    font-size: 39rpx;
+  line-height: 80rpx;
+
 }
 .the-information-buttons {
   margin-left: 10%; /* 设置左边距为屏幕宽度的10% */
@@ -137,16 +129,14 @@ export default {
   text-align: center;
   display: flex;
   line-height: 75rpx;
-  background-color: white;
+  background-color: #EFF3FF;
   border-radius: 20rpx;
   height: 125rpx
 }
 .down-page{
   margin-top: 20rpx;
   text-align: center;
-  background-color: white;
+  background-color: #EFF3FF;
 }
-.icon {
-  margin-right: 10rpx; /* 调整图标与文本之间的间距 */
-}
+
 </style>
