168216a3c1a06570880e82f00758721a5ba7de6b
阮梓杰 <qy2131467r@163.com>
2024-05-11 17:24:38 +0800

    修正代码

--- src/pages/mine/personal-information.vue
+++ src/pages/mine/personal-information.vue
@@ -1,14 +1,63 @@
 <template>
-  <view>
+
+  <view class="user">
+
+    <view class="yourhead">
+      <image class="head" @click="open" src="./static/store/lqj.jpg">
+      </image>
+
+      <uni-popup ref="a00" type="bottom">
+        <view class="popup">
+          <button class="choose">选择图片</button>
+          <button class="hide" @click="close">取消</button>
+        </view>
+      </uni-popup>
+
+    </view>
+
+    <view class="yourname">
+      用户名在此处
+    </view>
+
+    <view class="yourID">
+      用户ID在此处不可更改
+    </view>
 
   </view>
+
 </template>
 
 <script setup lang="ts">
+export default{
+  data(){
+    return
+  },
+  methods:{
+    open(){
+    this.$refs.popup.open('bottom')
+    },
+    close(){
+    this.$refs.popup.close()
+    }
+  }
+}
 
 </script>
 
-
 <style scoped lang="scss">
+.user{
 
+  background-color: white;
+}
+.head{
+  border: white;
+  border-radius: 180rpx;
+  width:  325rpx;
+  height: 325rpx;
+
+}
+.yourhead{
+  justify-content: center;
+  align-items: center;
+}
 </style>
