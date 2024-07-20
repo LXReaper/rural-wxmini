2606f0e9cd5ea3e2be5f99a9dfb0fade31138fd5
pyj1234 <1326126225@qq.com>
2024-07-20 10:51:55 +0800

    优化部分积分界面

---
+++ src/components/shop/hotGoodsView.vue
@@ -0,0 +1,58 @@
+<template>
+  <view>
+    <view class="product-card" @click="navigateToScorePage">
+      <image class="product-image" src="../static/images/notices/advertisements/金龙鱼油.jpg"></image>
+      <text class="product-title">商品标题</text>
+      <text class="product-description">商品描述</text>
+      <text class="product-price">￥99.99</text>
+    </view>
+  </view>
+</template>
+<script setup>
+// 跳转到积分商城页面
+const navigateToScorePage = () => {
+  uni.navigateTo(
+      {
+        url: "/pages/score/scoreHomePage"
+      }
+  )
+}
+</script>
+
+
+<style scoped lang="scss">
+/* 商品卡片 */
+.product-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.product-image {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  margin-bottom: 10px;
+}
+
+.product-title {
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.product-description {
+  font-size: 14px;
+  color: #666;
+  margin-bottom: 5px;
+}
+
+.product-price {
+  font-size: 16px;
+  color: #f00;
+}
+</style>
--- src/pages/score/score.vue
+++ src/pages/score/score.vue
@@ -4,48 +4,108 @@
     <view class="rounded-container">
       <!-- 六个小按钮 -->
       <view class="category-bar">
-      <view v-for="(category ,index) in categories" :key="index" @click="selectCategory(category)" :class="{active: category === selectedCategory}">
-        {{category}}
+        <view v-for="(category ,index) in categories" :key="index" @click="selectCategory(category)"
+              :class="{active: category === selectedCategory}">
+          {{ category }}
+        </view>
       </view>
+    </view>
+
+    <view
+        class="custom-button"
+        @click="onClick"
+    >
+      <view style="display: flex">
+        <view
+            style="
+            width: 13vw;
+            height: 8vh;
+            margin-top: -2vh;
+            background-image: url('https://2021539.s81i.faiusr.com/4/101/AFEIo7F7EAQYACDUksOBBijYz4fMBzA-ODpAZQ.png')
+            "
+        >
+        </view>
+        <view style="margin-left: 4vw">
+          <view style="font-size: 4.7vw;">积分商城</view>
+          <view style="font-size: 3vw">Shop</view>
+        </view>
       </view>
     </view>
     <!--下方四个按钮-->
-    <view style="display: flex">
-      <view></view>
+    <view style="background-color: white;margin: 1vw 0;height: 32vh;padding-top: 1vh">
+      <view style="display: flex;gap: 5vw;margin-left: 8vw;margin-top: 2vh;">
+        <view class="the-card" style="background: linear-gradient(to bottom, #FFDAB9, #FFC0CB);"
+              @click="LearningCenter">
+          <view class="title-wrap">
+            <view class="main-title">学习中心</view>
+            <view style="font-size: 12px; color: rgb(153, 153, 153);">学习技能</view>
+          </view>
+          <view class="material-icon"
+                style="background-image: url('https://17829202.s61i.faimallusr.com/4/AD0I0prACBAEGAAg6sSr3gUo5vCLlwEwUjhS.png');">
+            <view class="circle"/>
+          </view>
+        </view>
+        <view class="the-card" style="background: linear-gradient(to bottom, #FFE4B5, #FFd500);"
+              @click="onMissionCenter">
+          <view class="title-wrap">
+            <view class="main-title">任务中心</view>
+            <view style="font-size: 12px; color: rgb(153, 153, 153);">全部任务</view>
+          </view>
+          <view class="material-icon"
+                style="background-image: url('https://17829202.s61i.faimallusr.com/4/AD0I0prACBAEGAAgy8Sr3gUomKnhqgcwUjhS.png')">
+            <view class="circle"/>
+          </view>
+        </view>
+      </view>
+      <view style="display: flex;gap: 5vw;margin-left: 8vw;margin-top: 2vh;">
+        <view class="the-card" style="background: linear-gradient(to bottom, #9fe148, #78aa48);"
+              @click="navigateToCharts">
+          <view class="title-wrap">
+            <view class="main-title">排行榜&ensp;&ensp;</view>
+            <view style="font-size: 12px; color: rgb(153, 153, 153);">积分排行</view>
+          </view>
+          <view class="material-icon"
+                style="background-image: url('https://17829202.s61i.faimallusr.com/4/AD0I0prACBAEGAAgh8Wr3gUo8_ODywIwUjhS.png');">
+            <view class="circle"/>
+          </view>
+        </view>
+        <view class="the-card" style="background: linear-gradient(to bottom, #00FF7F, #40E0D0);">
+          <view class="title-wrap">
+            <view class="main-title">文明家庭</view>
+            <view style="font-size: 12px; color: rgb(153, 153, 153);">优秀家庭</view>
+          </view>
+          <view class="material-icon"
+                style="background-image: url('https://1102408.s81i.faiusr.com/4/101/AFEIyKRDEAQYACDhsKbwBSj1gIhaMFo4WkBl.png')">
+            <view class="circle"/>
+          </view>
+        </view>
+      </view>
     </view>
-    <view style="display:flex" id="button2">
-      <button class="button1" style="background-color: #04cefc" @click="LearningCenter">学习中心</button>
-      <button class="button2" style="background-color: #4cd964" @click="onMissionCenter">任务中心</button>
-      <button class="button3" style="background-color: #007aff" @click="navigateToCharts">排行榜 </button>
-      <button class="button4" style="background-color: #04fefc">文明家庭</button>
-    </view>
-    <button class="custom-button" @click="onClick">积分商城</button>
+
     <view class="recommend-text">{{ recommendMessage }}</view>
-<!--    热门商品推荐-->
+    <!--    热门商品推荐-->
     <view class="recommend-section">
       <text class="recommend-title">热门商品推荐</text>
       <!-- 商品卡片 -->
-<!--      点击卡片就进入积分商城页面-->
-      <view class="product-card" @click="navigateToScorePage">
-        <image class="product-image" src="/static/images/notices/advertisements/金龙鱼油.jpg"></image>
-        <text class="product-title">商品标题</text>
-        <text class="product-description">商品描述</text>
-        <text class="product-price">￥99.99</text>
-      </view>
+      <!--      点击卡片就进入积分商城页面-->
+      <hot-goods-view/>
       <!-- 添加更多商品卡片 -->
     </view>
   </scroll-view>
 </template>
 
-<script setup >
-import { ref,onMounted } from 'vue'
+<script setup>
+import {ref, onMounted} from 'vue'
 import {loginIntercept} from "../../utils/loginInterceptUtils";
 import {onShow} from "@dcloudio/uni-app";
-const categories = ref(['总览','卫生','贡献','任务','签到','考察']);
+import UniIcons from "../../uni_modules/uni-icons/components/uni-icons/uni-icons.vue";
+import HotGoodsView from "../../components/shop/hotGoodsView.vue";
+
+const categories = ref(['总览', '卫生', '贡献', '任务', '签到', '考察']);
 //存储选定的状态
 const selectedCategory = ref('总览');
-const selectCategory=(category)=>{
-selectedCategory.value=category;
+const selectCategory = (category) => {
+  selectedCategory.value = category;
 };
 // 点击商品卡片跳转到积分商城页面
 const onClick = () => {
@@ -53,57 +113,53 @@ const onClick = () => {
     url: "/pages/score/scoreHomePage",
   });
 };
-const  LearningCenter = () =>{
+const LearningCenter = () => {
   uni.navigateTo({
-    url:"/pages/score/LearningCenter"
+    url: "/pages/score/LearningCenter"
   });
 };
-const onMissionCenter=()=>{
+const onMissionCenter = () => {
   uni.navigateTo({
-    url:"/pages/score/MissionCenter"
+    url: "/pages/score/MissionCenter"
   })
 
 }
 const recommendMessage = '以下是推荐商品';
-// 跳转到积分商城页面
-const navigateToScorePage =()=>{
+
+const navigateToCharts = () => {
   uni.navigateTo(
       {
-        url:"/pages/score/scoreHomePage"
-      }
-  )
-}
-const navigateToCharts =()=>{
-  uni.navigateTo(
-      {
-        url:"/pages/score/Charts"
+        url: "/pages/score/Charts"
       }
   )
 }
 const currentPage = getCurrentPages()[getCurrentPages().length - 1];
 const currentRoute = currentPage.route;
-onShow(()=>{
-  loginIntercept("/"+currentRoute);
+onShow(() => {
+  loginIntercept("/" + currentRoute);
 })
 </script>
 
 <style scoped lang="scss">
-//上方按钮的样式
-.category-bar{
+/*上方按钮的样式*/
+.category-bar {
   display: flex;
   justify-content: space-around;
   padding: 10px;
-  background-color:#f8f8f8;
+  background-color: #f8f8f8;
 }
-.category-bar > view{
+
+.category-bar > view {
   cursor: pointer;
   padding: 10px;
 }
+
 .rounded-container {
   border-radius: 10px;
   padding: 10px;
   background-color: #f8f8f8;
 }
+
 .category-button {
   cursor: pointer;
   padding: 10px;
@@ -120,6 +176,54 @@ onShow(()=>{
   color: #007BFF; /* 选中状态颜色 */
   background-color: #e0e0e0;
 }
+
+/*四个卡片*/
+.the-card {
+  display: flex;
+  width: 40vw;
+  border: 1px solid #e2e2e2;
+  box-shadow: 10px 7px 6px rgba(0, 0, 0, 0.1);
+}
+
+.title-wrap {
+  height: 12vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 0 0 10px;
+}
+
+.main-title {
+  font-size: 4vw;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  width: 100%;
+  color: rgb(53, 53, 53);
+}
+
+.material-icon {
+  background-size: contain;
+  background-position: center center;
+  background-repeat: no-repeat;
+  display: inline-block;
+  vertical-align: top;
+  margin-left: 11vw;
+  padding-top: 3vh;
+  color: #fff;
+  text-align: left;
+}
+
+.circle {
+  width: 10vw;
+  height: 6vh;
+  border-radius: 50%;
+  padding-left: 1vw;
+  padding-top: 0.333vh;
+}
+
+/*按钮*/
 .button {
   width: auto; /* 自适应宽度 */
   height: 30px;
@@ -135,6 +239,7 @@ onShow(()=>{
 #button2 {
   display: flex;
 }
+
 .button1,
 .button2,
 .button3,
@@ -165,20 +270,27 @@ onShow(()=>{
 .button4 {
   background-color: #04fefc;
 }
+
 .custom-button {
   margin-top: 20px; /* 调整按钮与上方元素的间距 */
-  background-color: blue; /* 按钮背景颜色 */
   color: white; /* 按钮文字颜色 */
   border: none; /* 移除按钮边框 */
-  border-radius: 5px; /* 设置按钮圆角 */
-  padding: 10px 20px; /* 设置按钮内边距 */
-  font-size: 20px; /* 设置按钮文字大小 */
+  padding-left: 27vw;
+  padding-top: 0.9vh;
+  background-color: #4cd964;
+  border-radius: 45%;
+  width: 90vw;
+  height: 8vh;
+  margin-left: 5vw;
+  margin-right: 5vw;
 }
+
 .recommend-text {
   text-align: center;
   font-size: 18px;
   margin-top: 20px; /* 根据需要调整上边距 */
 }
+
 .recommend-section {
   padding: 20px;
   background-color: #f9f9f9;
@@ -191,40 +303,6 @@ onShow(()=>{
   margin-bottom: 10px;
 }
 
-/* 商品卡片 */
-.product-card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: 1px solid #ddd;
-  border-radius: 10px;
-  padding: 10px;
-  margin-bottom: 10px;
-}
-
-.product-image {
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-  margin-bottom: 10px;
-}
-
-.product-title {
-  font-size: 16px;
-  font-weight: bold;
-  margin-bottom: 5px;
-}
-
-.product-description {
-  font-size: 14px;
-  color: #666;
-  margin-bottom: 5px;
-}
-
-.product-price {
-  font-size: 16px;
-  color: #f00;
-}
 </style>
 
 }
